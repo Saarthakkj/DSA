775d8f229414a73ee9b83553378abaa42b333118
Saarthak Saxena <119070811+Saarthakkj@users.noreply.github.com>
2024-03-02 05:22:04 +0530

    Time: 8 ms (76.78%) | Memory: 22.3 MB (41.75%) - LeetSync

--- Leetcode_problems/stacks_queues/42-trapping-rain-water/trapping-rain-water.cpp
+++ Leetcode_problems/stacks_queues/42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     int trap(vector<int>& v) {
+        //space complexity  = 2N
+
         int water = 0 ;
-        vector<int> prefix; 
-        int l_max = 0;
-        for(int i = 0 ; i< v.size()-1 ; i++){
-            if(v[i] > l_max){
-                l_max= v[i];
+        int l_max =0 ; 
+        int r_max =  0 ;
+        int l = 0;
+        int r = v.size()-1 ;
+        while(l<=r){
+            if(v[l] <= v[r]){
+                if(v[l] >= l_max ){
+                    l_max = v[l];
+                }
+                else{
+                    water+=(l_max -v[l]);
+                }
+                l++;
             }
-            //cout<< "l_max : " << l_max << endl;
-            prefix.push_back(l_max);
-        }
-
-        vector<int> suff ; 
-        int r_max  = 0 ;
-        for(int i =  v.size()-1 ; i>=0 ; i--){
-            if(v[i] > r_max ){
-                r_max = v[i];
+            else{
+                if(v[r] >= r_max){
+                    r_max = v[r];
+                }
+                else{
+                    water+=(r_max - v[r]);
+                }
+                r--;
             }
-            //cout<< "r_max : " << r_max << endl;
-            suff.insert(suff.begin() ,r_max);
         }
-
-        for(int i = 0 ; i < v.size()-1 ; i++){
-            int temp = v[i] ;
-            water+= min(prefix[i] , suff[i]) - temp;
-        }
-
         return water;
     }
 };
