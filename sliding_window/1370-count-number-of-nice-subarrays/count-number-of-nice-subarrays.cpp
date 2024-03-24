de0d424e2e5d0723e12885cfe85e36678661c0ac
Saarthak Saxena <119070811+Saarthakkj@users.noreply.github.com>
2024-03-24 22:56:29 +0530

    Time: 269 ms (5.49%) | Memory: 121.7 MB (7.70%) - LeetSync

---
+++ sliding_window/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int func( vector<int>& arr ,  int k ){
+        map<int,int> mpp ; 
+        int l = 0 ;
+        int r  = 0 ;
+        int ans= 0  ; 
+        int oddcount = 0 ;
+        while(r< arr.size()){
+            if(arr[r]%2){
+                //odd number 
+                oddcount ++ ; 
+                while(oddcount >k){
+                    if(arr[l] %2) { 
+                        oddcount-- ; 
+                    }
+                    l++ ; 
+                }
+                mpp[r] =  oddcount ; 
+            }
+            ans+= (r-l+1);
+            //cout<< "r : "<< r << " l : "<< l << endl;
+            r++ ; 
+        }
+        //cout<< "ans : "<< ans << endl;
+        return ans ;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return func(nums , k) - func(nums , k-1) ;
+        
+    }
+};
