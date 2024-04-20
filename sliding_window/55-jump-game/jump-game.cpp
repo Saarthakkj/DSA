8d3849ec9d398379328c784e34663a6dcda327b7
Saarthak Saxena <119070811+Saarthakkj@users.noreply.github.com>
2024-04-20 22:21:18 +0530

    Time: 44 ms (61.45%) | Memory: 50.9 MB (23.06%) - LeetSync

--- sliding_window/55-jump-game/jump-game.cpp
+++ sliding_window/55-jump-game/jump-game.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
    bool canJump(vector<int>& nums) {
-        int lastPos = nums.size() - 1; // Index of the last element
+      int goal = nums.size()-1 ;
 
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            if (i + nums[i] >= lastPos) {
-                lastPos = i; // Update the last position that can reach the end
-            }
+      for(int i = nums.size()-1 ; i >= 0 ; i--){
+        if(i+nums[i] >= goal){
+            goal = i ; 
         }
+      }
 
-        return lastPos == 0; // Check if we can reach the start from the last position
+        return (goal == 0 ); 
     }
-    // 
-    
 };
