68e1f7f991b9e02a053e6ad40e53079e2e7ff117
Saarthak Saxena <119070811+Saarthakkj@users.noreply.github.com>
2024-04-27 09:51:28 +0530

    Time: 3 ms (70.16%) | Memory: 17.4 MB (13.28%) - LeetSync

---
+++ sliding_window/101-symmetric-tree/symmetric-tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool check(TreeNode* root1 , TreeNode* root2){
+        if(root1 == NULL and root2 == NULL){
+            return true;
+        }
+        else if(root1 == NULL or root2 == NULL){
+            return false;
+        }
+
+        if(root1->val == root2->val){
+            if(check(root1->left, root2->right) and check(root1->right , root2->left)){
+                return true; 
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+
+        return true;
+    }
+    bool isSymmetric(TreeNode* root) {
+        return check(root->left , root->right) ;
+    }
+};
