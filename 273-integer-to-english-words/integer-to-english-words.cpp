42e17d09b876855fd939d124f412991bd9a66324
Saarthak Saxena <119070811+Saarthakkj@users.noreply.github.com>
2024-08-07 19:54:20 +0530

    Time: 8 ms (14.19%) | Memory: 11.4 MB (60.46%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.cpp
+++ 273-integer-to-english-words/integer-to-english-words.cpp
@@ -15,7 +15,7 @@ public:
         vector<string> ten = {" Ten", " Eleven", " Twelve", " Thirteen", " Fourteen", " Fifteen", " Sixteen", " Seventeen", " Eighteen", " Nineteen"};
 
         for (int i = words.size() - 1; i >= 0; i--) {
-           // cout<< " words[i] : "<< words[i] << " res : "<< res << "i : "<< i << endl;
+            cout<< " words[i] : "<< words[i] << " res : "<< res << "i : "<< i << endl;
             if (i == 0) {
                 res += ones[words[i] - '0'];
                 break;
@@ -26,16 +26,16 @@ public:
                 // if i%3 == 0
                 // index is either thousandth, millionth, billionth position
                 //CAUTION : shouldn't execute when i = 3, 6 , 9 and words[i] ==0
-
+                res += ones[words[i] - '0'] ;
                 switch (i) {
                     case 3:
-                        res += ones[words[i] - '0'] + " Thousand";
+                        res+= " Thousand";
                         break;
                     case 6:
-                        res += ones[words[i] - '0'] + " Million";
+                        res += " Million";
                         break;
                     case 9:
-                        res += ones[words[i] - '0'] + " Billion";
+                        res += " Billion";
                         break;
                 }
 
@@ -44,39 +44,27 @@ public:
                 // if index is just before i%3==0 index
                 // i.e. 2, 5, 8
                 // then hundred + x is the position
+                res += ones[words[i] - '0'] + " Hundred";
+                if (words[i - 1] != '1') {
+                    res += tens[words[i - 1] - '0'] + ones[words[i - 2] - '0'];
+                } else {
+                    res += ten[words[i - 2] - '0'];
+                }
                 switch (i) {
                     case 2:
-                        res += ones[words[i] - '0'] + " Hundred";
-                        if (words[i - 1] != '1') {
-                            res += tens[words[i - 1] - '0'] + ones[words[i - 2] - '0'];
-                        } else {
-                            res += ten[words[i - 2] - '0'];
-                        }
-                        i -= 2;
+                        res+="";
+                        //no such thing as twelve hundred . it's one thousand two hundred.
                         break;
                     case 5:
-                        res += ones[words[i] - '0'] + " Hundred";
-                        if (words[i - 1] != '1') {
-                            res += tens[words[i - 1] - '0'] + ones[words[i - 2] - '0'];
-                        } else {
-                            res += ten[words[i - 2] - '0'];
-                        }
                         res += " Thousand";
-                        i -= 2;
+                        //i -= 2;
                         break;
                     case 8:
-                        res += ones[words[i] - '0'] + " Hundred";
-                        if (words[i - 1] != '1') {
-                            res += tens[words[i - 1] - '0'] + ones[words[i - 2] - '0'];
-                        } else {
-                            res += ten[words[i - 2] - '0'];
-                        }
                         res += " Million";
-                        i -= 2;
+                        // -= 2;
                         break;
-                    default :
-                        res+="";
                 }
+                i -= 2;
             } else {
                 // if neither
                 // then tenth's position
@@ -85,43 +73,29 @@ public:
 
                 if (words[i] == '1') {
                     res += ten[words[i - 1] - '0'];
-                    i -= 1;
-
-                    // select position for 11...19
-                    switch (i) {
-                        case 0:
-                            break;
-                        case 3:
-                            res += " Thousand";
-                            break;
-                        case 6:
-                            res += " Million";
-                            break;
-                        default :
-                            res+="";
-                            // for words[i] == 0 
-                    }
-                    continue;
+                }
+                else{
+                    res += tens[words[i] - '0'];
+                    res+= ones[words[i-1] - '0'];
                 }
                 // for tens like: "forty-seven"
-                res += tens[words[i] - '0'];
-                res+= ones[words[i-1] - '0'];
+                
                 i-=1;
                  // select position for 11...19
-                    switch (i) {
-                        case 0:
-                            break;
-                        case 3:
-                            res += " Thousand";
-                            break;
-                        case 6:
-                            res += " Million";
-                            break;
-                        default :
-                            res+="";
-                            // for words[i] == 0 
-                    }
-                    continue;
+                switch (i) {
+                    case 0:
+                        break;
+                    case 3:
+                        res += " Thousand";
+                        break;
+                    case 6:
+                        res += " Million";
+                        break;
+                    default : 
+                        res+="";
+                        break;
+                }
+                continue;
             }
         }
         // to truncate first space
