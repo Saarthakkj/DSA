ab5119b42a23fa50fe13c1fc0b06dfa2fe034cfc
Saarthak Saxena <119070811+Saarthakkj@users.noreply.github.com>
2024-08-22 15:15:19 +0530

    Time: 3 ms (41.07%) | Memory: 7.6 MB (33.08%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int n = log2(num);
+        for(int i = 0 ; i <=n ; i++){
+            num  = num^ (1 << i);
+        }
+        return num;
+    }
+};
